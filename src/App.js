3ff88c4259e241a7256fdb9a9bf8229630e64b2f
kyriakidisdimitrios <kyriakidis.demetrios@gmail.com>
2024-07-16 00:29:37 +0300

    feat: minor UI fixes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Header from './components/Header';
 import CollapsibleSection from './components/CollapsibleSection/CollapsibleSection';
 import Summary from './components/Summary';
 import Skills from './components/Skills';
-import WorkExperience from './components/WorkExperience';
 import Education from './components/Education';
 import AdditionalInfo from './components/AdditionalInfo/AdditionalInfo'; // Import AdditionalInfo component
 
@@ -19,9 +18,6 @@ function App() {
             </CollapsibleSection>
             <CollapsibleSection title="Skills">
                 <Skills />
-            </CollapsibleSection>
-            <CollapsibleSection title="Work Experience">
-                <WorkExperience />
             </CollapsibleSection>
             <CollapsibleSection title="Education">
                 <Education />
